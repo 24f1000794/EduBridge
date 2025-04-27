edc8d99aca8fd8636abafbae9c4cf4110a8682cf
pranshubatham78 <24f1000794@ds.study.iitm.ac.in>
2025-04-27 19:30:19 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,2 @@
 from .database import db
-from .user import User, Course,Mentor,Module,Messages,Quiz,Note,Question,QuizAttempt,Option,Answer,Badge,SubModule
+from .user import User, Course,Mentor,Module,Quiz,Note,Question,QuizAttempt,Option,Answer,Badge,SubModule,Messages,MentorMessages
